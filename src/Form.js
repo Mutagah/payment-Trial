8892fa61a4df517488c24ef371c1955deff17a4c
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-10-29 19:18:57 +0300

    Trial on payment

--- src/Form.js
+++ src/Form.js
@@ -1,21 +1,50 @@
 import React, { useState } from "react";
 function Form() {
     const [formData, setFormData] = useState({})
+    let date = new Date();
+
+  let timestamp = date.getFullYear() +
+    ("0" + (date.getMonth() + 1)).slice(-2) +
+    ("0" + date.getDate()).slice(-2) +
+    ("0" + date.getHours()).slice(-2) +
+    ("0" + date.getMinutes()).slice(-2) +
+    ("0" + date.getSeconds()).slice(-2);
+
     function handleChange(event){
         setFormData({...formData,[event.target.name]:event.target.value})
     }
-    console.log(formData)
     function handleSubmit(event){
         event.preventDefault()
-        fetch("http://localhost:3000/peyment",{
-        method: "POST",
-        headers: {
-        "Content-Type" : "application/json"
-      },
-      body :JSON.stringify(formData)
-    }).then((response)=> response.json())
-    .then((data)=> console.log(data))
+    //     fetch("http://localhost:3000/peyment",{
+    //     method: "POST",
+    //     headers: {
+    //     "Content-Type" : "application/json"
+    //   },
+    //   body :JSON.stringify(formData)
+    // }).then((response)=> response.json())
+    // .then((data)=> console.log(data))
+
+    fetch("https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest",{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      }, body :JSON.stringify({
+        BusinessShortCode:"174379",    
+        Password: btoa( "174379" + "4LymgXU3bRPSNGn7Dxe36oUxoWvIKeGt" +timestamp),    
+        Timestamp:timestamp,    
+        TransactionType: "CustomerPayBillOnline",    
+        Amount:"1",    
+        PartyA:"254727249154",    
+        PartyB:"174379",    
+        PhoneNumber:"254727249154",    
+        CallBackURL:"https://payment-trial-mpay.herokuapp.com/",    
+        AccountReference:"ETickets",    
+        TransactionDesc:"ETickets" 
+      })
+    }).then (response => response.json()).then((data)=> console.log(data))
 }
+console.log(timestamp)
+console.log(btoa( "174379" + "4LymgXU3bRPSNGn7Dxe36oUxoWvIKeGt" +timestamp));
   return (
     <>
       <form>
